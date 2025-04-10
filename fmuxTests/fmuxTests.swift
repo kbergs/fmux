4af2162eff9ba5459990b1bc986ac0aa8191a4b5
kbergs <kylebergstedt@live.com>
2025-04-09 22:13:18 -0700

    Initial Commit

---
+++ fmuxTests/fmuxTests.swift
@@ -0,0 +1,17 @@
+//
+//  fmuxTests.swift
+//  fmuxTests
+//
+//  Created by Kyle Bergstedt on 4/9/25.
+//
+
+import Testing
+@testable import fmux
+
+struct fmuxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
